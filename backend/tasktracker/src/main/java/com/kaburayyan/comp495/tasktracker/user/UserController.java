ecddda1749aba2e4b875304574518a49b8e04574
Justin Mosley <justinmosley@proton.me>
2024-10-10 13:44:31 -0400

    Adds backend CRUD endpoints without secure features.

--- backend/tasktracker/src/main/java/com/kaburayyan/comp495/tasktracker/user/UserController.java
+++ backend/tasktracker/src/main/java/com/kaburayyan/comp495/tasktracker/user/UserController.java
@@ -1,8 +1,14 @@
 package com.kaburayyan.comp495.tasktracker.user;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/v1")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
@@ -20,7 +26,7 @@ public class UserController {
     }
 
     @RequestMapping(path = "/user", method = RequestMethod.POST)
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody User user) {
         if (user.getEmail() == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -29,9 +35,63 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        this.userService.addUser(user);
+        User createUser = this.userService.addUser(user);
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", createUser.getId());
+        response.put("username", createUser.getUsername());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserById(@PathVariable UUID id){
+
+        User user = this.userService.getUserById(id);
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    @RequestMapping(path = "user/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Map<String, Object>> updateUser(@RequestBody Map<String, Object> updates){
+        
+        UUID id = UUID.fromString(updates.get("id").toString());
+        String username = (String) updates.get("username");
+        String email = (String) updates.get("email");
+        String password = (String) updates.get("password"); //this will need to be updated for security purposes
+        
+        User updatedUser = userService.updateUser(id, username, email, password);
+        if (updatedUser == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("id", updatedUser.getId());
+        responseBody.put("username", updatedUser.getUsername());
+
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Map<String, Object>> deleteUser(@PathVariable UUID id){
+       Optional<User> deletedUserOptional = this.userService.deleteUser(id);
+
+        if(deletedUserOptional.isPresent()){
+            User deletedUser = deletedUserOptional.get();
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("id", deletedUser.getId());
+            responseBody.put("username", deletedUser.getUsername());
+    
+            return new ResponseEntity<>(responseBody, HttpStatus.OK); 
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+      
+    }
+
+
 
 
 }
--- backend/tasktracker/src/main/java/com/kaburayyan/comp495/tasktracker/user/UserService.java
+++ backend/tasktracker/src/main/java/com/kaburayyan/comp495/tasktracker/user/UserService.java
@@ -1,5 +1,7 @@
 package com.kaburayyan.comp495.tasktracker.user;
 
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,37 @@ public class UserService {
     public User addUser(User user) {
         this.repository.save(user);
         return user;
+    }
+
+    public User getUserById(UUID id){
+        Optional<User> user = repository.findById(id);
+        return user.orElse(null);
+    }
+
+    public User updateUser(UUID id, String username, String email, String password){
+        Optional<User> user = repository.findById(id);
+        
+        if(user.isPresent()){
+            //set for existing user, this variable is for readabillity
+            User existingUser = user.get();
+
+            existingUser.setUsername(username);
+            existingUser.setEmail(email);
+            existingUser.setPassword(password);
+
+            return repository.save(existingUser);
+        } else {
+            return null; //user not found
+        }
+    }
+
+    public Optional<User> deleteUser(UUID id){
+        Optional<User> user = repository.findById(id);
+        if(user.isPresent()){
+            repository.deleteById(id);
+        } 
+
+        return user;
+        
     }
 }
